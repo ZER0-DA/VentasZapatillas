136fbce87c73e26666a4b319b6346ff3212ff22e
Anthony.C <castilloanthony5215@gmail.com>
2025-11-14 15:12:17 -0500

    Agregando el modelo para Usuario

---
+++ VentasZapatillasApi/Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace ventasZapatiilasAPI.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public  required string Email { get; set; }
+        public required string Password { get; set; }
+
+
+    }
+}
